1c8ac4356bec78cb861cf13e55cd29e8bc020be7
HtetAungWai <htetaungwai1994@gmail.com>
2024-04-10 14:34:23 +0630

    This is the second commit of HAWHAW

--- main.py
+++ main.py
@@ -3,7 +3,15 @@
 def add(num1, num2):
     result = num1+ num2
     print("The result is:", result)
-    
+
+def divide(num1,num2):
+    result= num1/num2
+    print("The result is:",result)
+
+def modulo(num1,num2):
+    result= num1%num2
+    print("The result is :", result) 
+
 def main():
     print("Enter the operation you want to perform: ")
 
